4d327632bb449c6b087a7ce1695aabd99aadcac2
Matthias Thym <git@thym.at>
2022-02-19 18:02:39 +0100

    Update theme and instructions
    - Renamed theme to title case to fit default LibreOffice themes
    - Make scripts POSIX-compliant
    - Add install script for one-click-install
    - Add screenshots to installation instructions
    - Add myself (totoroot) as contributor

--- README.md
+++ README.md
@@ -12,9 +12,9 @@ All instructions can be found at [draculatheme.com/libreoffice](https://draculat
 
 This theme is maintained by the following person(s) and a bunch of [awesome contributors](https://github.com/dracula/libreoffice/graphs/contributors).
 
-[![Tim Clifford](https://github.com/tim-clifford.png?size=100)](https://github.com/tim-clifford) |
---- |
-[Tim Clifford](https://github.com/tim-clifford) |
+[![Tim Clifford](https://github.com/tim-clifford.png?size=100)](https://github.com/tim-clifford) | [![Matthias Thym](https://github.com/totoroot.png?size=100)](https://github.com/totoroot) |
+--- | --- |
+[Tim Clifford](https://github.com/tim-clifford) | [Matthias Thym](https://github.com/totoroot) |
 
 ## License
 
---
+++ add-dracula-application-colors.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env sh
+
+# Check OS platform
+if [ "$(uname)" = "Linux" ]; then
+	fname=$XDG_CONFIG_HOME/libreoffice/*/user/registrymodifications.xcu
+elif [ "$(uname)" = "Darwin" ]; then
+	fname=$HOME/Library/Application Support/LibreOffice/*/user/config/
+else
+	echo "Unsupported kernel. Aborting ..."
+	exit 1
+fi
+
+# Expand filename
+fname=$(realpath $fname)
+
+# Create backup of LibreOffice registry before modifications
+cp "$fname" registrymodifications.xcu.bak
+
+# Check settings file
+if ! [ -f "$fname" ]; then
+	echo "Settings file doesn't exist in expected location. Aborting ..."
+	exit 1
+elif ! tail -n1 "$fname" | grep -E -q '^</oor:items>$'; then
+	echo "Settings file doesn't match expected format. Aborting ..."
+	exit 1
+fi
+
+# Check if color scheme it present already
+existing_theme="$(grep -n \
+	'<item oor:path="/org.openoffice.Office.UI/ColorScheme/ColorSchemes"><node oor:name="Dracula"' \
+	$fname)"
+exit_code=$?
+theme_line="$(echo "$existing_theme" | sed 's|:.*||')"
+
+if [ $exit_code -eq 0 ]; then
+	echo "Dracula theme appears to already be installed. Replacing it ..."
+	# Replace existing line with
+	settings_start="$(head -n $theme_line $fname | head -n -1)"
+	settings_end="$(tail -n +$theme_line $fname | tail -n +2)"
+	new_settings="$(echo "$settings_start" \
+	             && cat dracula.xcu \
+				 && echo "$settings_end")"
+else
+	# Insert theme between last two lines if nof present
+	new_settings="$(head -n -1 $fname && cat dracula.xcu && tail -n1 $fname)"
+fi
+
+# Write new settings to settings file
+echo "$new_settings" > "$fname"
