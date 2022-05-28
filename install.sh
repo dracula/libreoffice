9a2f47885789fb92daa80532af356aee9ae00720
ooonea.eth <35407790+ooonea@users.noreply.github.com>
2022-05-28 22:14:58 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,10 @@
 echo "Copying palette to config directory ..."
 cp dracula.soc "${XDG_CONFIG_HOME:-$HOME/.config}"/libreoffice/*/user/config/
 
-echo "Running add-dracula-application-colors.sh ..."
-./add-dracula-application-colors.sh
+if [ "$(uname)" = "FreeBSD" ]; then
+	echo "Running add-dracula-application-colors.sh ..."
+	./add-FreeBSD-dracula-application-colors.sh
+else
+	echo "Running add-dracula-application-colors.sh ..."
+	./add-dracula-application-colors.sh
+fi
